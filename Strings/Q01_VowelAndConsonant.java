0262a684ba86c0306bfb7c5ea4bd6959e5bc90fd
Ankur Verma <ankurverma4574@gmail.com>
2026-08-18 15:44:15 +0530

    Create Q01_VowelAndConsonant.java

---
+++ Strings/Q01_VowelAndConsonant.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+        int vowels = 0;
+        int consonant = 0;
+        for (int i = 0 ; i < str.length(); i++){
+            char ch = Character.toLowerCase(str.charAt(i));
+            if (ch == ' '){
+                continue;
+            }
+            else if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+               vowels++;
+            }
+            else {
+               consonant++;
+            }
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonant: " + consonant);
+    }
+}
