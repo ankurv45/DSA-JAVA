6b7884479db7092d80c6d87bd52a716419afcb0a
Ankur Verma <ankurverma4574@gmail.com>
2026-08-19 12:17:10 +0530

    Update Q03__RemoveDuplicateCharacters.java

--- Strings/Q03__RemoveDuplicateCharacters.java
+++ Strings/Q03__RemoveDuplicateCharacters.java
@@ -1,3 +1,5 @@
+// 1st way
+
 class RemoveDuplicateCharacters{
     public static void main(String[] args) {
 
@@ -15,6 +17,25 @@ class RemoveDuplicateCharacters{
                 }
             }
             System.out.print(ch[i]);
+        }
+    }
+}
+
+
+//2nd way
+
+class Main {
+    public static void main(String[] args) {
+
+        String str = "programming";
+        int count[] = new int[256];
+
+        for (int i = 0; i < str.length();i++){
+            char ch = str.charAt(i);
+            if (count[ch] == 0){
+                System.out.println(ch);
+                count[ch] = 1;
+            }
         }
     }
 }
