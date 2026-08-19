9af07898b7f22554aa0bf419b51cbc74c4af8ffa
Ankur Verma <ankurverma4574@gmail.com>
2026-08-19 16:47:57 +0530

    Create Q10__FirstNonRepeatingCharacter.java

---
+++ Strings/Q10__FirstNonRepeatingCharacter.java
@@ -0,0 +1,19 @@
+class FirstNonRepeatingCharacter{
+    public static void main(String[] args) {
+
+        String str = "swiss";
+        int count[] = new int[256];
+
+        for (int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            count[ch]++;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (count[ch] == 1) {
+                System.out.println(ch);
+                break;
+            }
+        }
+    }
+}
