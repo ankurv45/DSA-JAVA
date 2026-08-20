6e07c780a298ba66729d475b10ec9cd3393e6f97
Ankur Verma <ankurverma4574@gmail.com>
2026-08-20 23:16:30 +0530

    Create Q13__StringRotation.java

---
+++ Strings/Q13__StringRotation.java
@@ -0,0 +1,21 @@
+public class StringRotation {
+
+    public static void main(String[] args) {
+
+        String str1 = "abcd";
+        String str2 = "cdab";
+
+        boolean isRotation = false;
+
+        if (str1.length() == str2.length()) {
+
+            String combined = str1 + str1;
+
+            if (combined.contains(str2)) {
+                isRotation = true;
+            }
+        }
+
+        System.out.println("Is Rotation: " + isRotation);
+    }
+}
