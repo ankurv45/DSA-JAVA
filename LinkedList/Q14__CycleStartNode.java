8e2904a3586ab4828ae481f0d1f7f88046104617
Ankur Verma <ankurverma4574@gmail.com>
2026-08-26 23:05:47 +0530

    Create Q14__CycleStartNode.java

---
+++ LinkedList/Q14__CycleStartNode.java
@@ -0,0 +1,45 @@
+class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+}
+class CycleStartNode {
+    public static void main(String[] args) {
+
+     Node n1 = new Node(10);
+     Node n2 = new Node(20);
+     Node n3 = new Node(30);
+     Node n4 = new Node(40);
+     Node n5 = new Node(50);
+
+     Node head = n1;
+
+     n1.next = n2;
+     n2.next = n3;
+     n3.next = n4;
+     n4.next = n5;
+
+     n5.next = n3;
+     
+     Node slow = head;
+     Node fast = head;
+
+     while (fast != null && fast.next != null) {
+         slow = slow.next;
+         fast = fast.next.next;
+         if (slow == fast) {
+             slow =head;
+             while (slow != fast) {
+                 slow = slow.next;
+                 fast = fast.next;
+             }
+             break;
+         }
+     }
+        System.out.println(slow.data);
+
+    }
+}
