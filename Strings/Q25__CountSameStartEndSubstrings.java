b27c3c414cd01dcfd9322c8c9213d981df1e0e24
Ankur Verma <ankurverma4574@gmail.com>
2026-09-01 23:18:08 +0530

    Create Q25__CountSameStartEndSubstrings.java

---
+++ Strings/Q25__CountSameStartEndSubstrings.java
@@ -0,0 +1,21 @@
+class CountSameStartEndSubstrings {
+
+    public static void main(String[] args) {
+
+        String str = "abcab";
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            for (int j = i; j < str.length(); j++) {
+
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Count: " + count);
+    }
+}
